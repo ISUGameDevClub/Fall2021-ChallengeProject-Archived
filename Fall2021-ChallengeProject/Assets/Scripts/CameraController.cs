f684b6441bf4e3fb237399ae42447c4a79d5cd30
codydoerr <40671276+codydoerr@users.noreply.github.com>
2021-09-17 00:53:33 -0500

    Added Shadow Casters, Added a point light, added global light and player light

---
+++ Fall2021-ChallengeProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerTr;
+    public Camera camera;
+    public float cameraDistance = 10f;
+    Vector3 cameraPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraPosition = new Vector3(playerTr.position.x,playerTr.position.y,-cameraDistance);
+        camera = gameObject.GetComponent<Camera>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraPosition = new Vector3(playerTr.position.x, playerTr.position.y, -cameraDistance);
+        transform.position = cameraPosition;
+    }
+}
